ce77b36eb6657243bcd4780f52e5871ee57fa1e5
RidaTawil <82220002@students.liu.edu.lb>
2025-12-30 19:44:32 +0200

    Force PostgreSQL driver, remove MySQL driver config

--- src/main/java/com/gym/gym_backend/GymBackendApplication.java
+++ src/main/java/com/gym/gym_backend/GymBackendApplication.java
@@ -2,11 +2,21 @@ package com.gym.gym_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class GymBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GymBackendApplication.class, args);
+
+		SpringApplication app = new SpringApplication(GymBackendApplication.class);
+
+		Map<String, Object> props = new HashMap<>();
+		props.put("server.address", "0.0.0.0");
+		props.put("server.port", "8080");
+
+		app.setDefaultProperties(props);
+		app.run(args);
 	}
 }
